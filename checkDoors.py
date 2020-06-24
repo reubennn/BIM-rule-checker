97336e26f233170a5f4d2e92f8e149efcdc2294e
Reuben Smith <reuben-smith@hotmail.com>
2020-06-24 14:58:36 +0930

    Changes method to finding the IFCDOOR height
    * Finds the height in the 9th property of the IFCDOOR
    * Removes previous method code
    * As a result, most of the code nests within the main section

--- checkDoors.py
+++ checkDoors.py
@@ -1,11 +1,11 @@
+import re # For regexp
+
 file = open("./Sample Mockup Model.ifc","r")
 
-ifcDoorStr = "IFCDOOR"
+ifcDoorStr = "IFCDOOR("
 passed = True
-warning = False
 lineMatch = []
 lineNumber = []
-warningList = []
 failedList = []
 passedList = []
 
@@ -19,59 +19,42 @@ for lineNum, data in enumerate(file, 1):
   line = data.splitlines()
 
   if any(ifcDoorStr in s for s in line): # Search for ifcDoorStr in line
-    lineNumber.append(int(lineNum))
-    lineMatch.append(line[0])
+    tempLine = line[0].upper()
+    try:
+        ifcNum = int(tempLine[1:tempLine.index(" ")]) # Extract IFC #
+    except ValueError:
+        ifcNum = "NONE"
 
-for i, line in enumerate(lineMatch, 1):
-  tempLine = line.upper()
-  ifcNum = tempLine[0:line.index(" ")]
-  try:
-    index = tempLine.index(" X ")
-  except ValueError: # If not found, return -1
-    index = -1
+    height = findWithProperty(tempLine, 9)
 
-  if index != -1:
-    index += 3
-    height = int(line[index: index + 4])
     if height >= 2000:
-      print("*PASSED* [LINE = {}] [IFC {}] Height = {}".format(lineNumber[i - 1], ifcNum, height))
-      passedList.append("*PASSED* [LINE = {}] [IFC {}] Height = {}".format(lineNumber[i - 1], ifcNum, height))
+      print("*PASSED* [LINE = {}] [IFC {}] : Height = {}".format(lineNum, ifcNum, height))
+      passedList.append("*PASSED* [LINE = {}] [IFC #{}] Height = {}".format(lineNum, ifcNum, height))
     else:
-      print("*FAILED* [LINE = {}] [IFC {}] Height = {}".format(lineNumber[i - 1], ifcNum, height))
+      print("*FAILED* [LINE = {}] [IFC #{}] : Height = {}".format(lineNum, ifcNum, height))
       passed = False
-      failedList.append("[LINE = {}] [IFC {}] Height = {}".format(lineNumber[i - 1], ifcNum, height))
-  else:
-    print("*WARNING* [LINE = {}] [IFC {}] No Height specified".format(lineNumber[i - 1], ifcNum))
-    warning = True
-    warningList.append("[LINE = {}] [IFC {}] No Height specified".format(lineNumber[i - 1], ifcNum))
+      failedList.append("[LINE = {}] [IFC #{}] : Height = {}".format(lineNum, ifcNum, height))
 
 # Print Results
-if passed and warning:
-  print("\r\nRule 8 PASSED, but with WARNINGS.\r\nThe following PASSED \r\n{")
-  for i, item in enumerate(passedList, 1):
-    print("     {}".format(item))
-    if i == len(passedList):
-      print("}")
-  print("\r\nThe following were WARNINGS \r\n{")
-  for i, item in enumerate(warningList, 1):
-    print("     {}".format(item))
-    if i == len(warningList):
-      print("}")
-  print("\r\nRule 8 PASSED, but with WARNINGS. Scroll up to see details.")
-elif passed:
+if passed:
   print("\r\nRule 8 PASSED with the following:")
   for i, item in enumerate(passedList, 1):
     print("     {}".format(item))
     if i == len(passedList):
       print("}")
-  print("\r\nRule 8 PASSED. Scroll up for details.")
+  print("\r\nRule 8 PASSED. Scroll up for details.\r\n")
 else:
-  print("\r\nRule 8 FAILED for the following \r\n{")
+  print("\r\nRule 8 FAILED. These doors PASSED:\r\n{")
+  for i, item in enumerate(passedList, 1):
+    print("     {}".format(item))
+    if i == len(passedList):
+      print("}")
+  print("\r\nThese doors FAILED:\r\n{")
   for i, item in enumerate(failedList, 1):
     print("     {}".format(item))
     if i == len(failedList):
       print("}")
-  print("\r\nRule 8 FAILED. Scroll up for details.")
+  print("\r\nRule 8 FAILED. Scroll up for details.\r\n")
 
 # Close the file to free up resources
 file.close()
