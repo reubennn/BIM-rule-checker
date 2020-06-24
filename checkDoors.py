d1e07096126fffe4eb72b3b833c8216c7e1f0b28
Reuben Smith <reuben-smith@hotmail.com>
2020-06-24 14:28:59 +0930

    Finds the height of the doors and determines if they meet the check
    * Finds the height of the doors using the " X " property
    * After viewing file in BIM Vision, turns out this is not the actual height
    * This is a commit before making changes to fix the code to find the right height

--- checkDoors.py
+++ checkDoors.py
@@ -7,6 +7,13 @@ lineMatch = []
 lineNumber = []
 warningList = []
 failedList = []
+passedList = []
+
+def findWithProperty(line, propertyNum):
+    # print("Searching for property {} in: {}".format(propertyNum, line))
+    properties = line.split(",")
+    valueToFind = float(re.sub("[^0-9,.]", "", properties[propertyNum - 1])) # Find ifc using regexp and the property number
+    return valueToFind
 
 for lineNum, data in enumerate(file, 1):
   line = data.splitlines()
@@ -28,6 +35,7 @@ for i, line in enumerate(lineMatch, 1):
     height = int(line[index: index + 4])
     if height >= 2000:
       print("*PASSED* [LINE = {}] [IFC {}] Height = {}".format(lineNumber[i - 1], ifcNum, height))
+      passedList.append("*PASSED* [LINE = {}] [IFC {}] Height = {}".format(lineNumber[i - 1], ifcNum, height))
     else:
       print("*FAILED* [LINE = {}] [IFC {}] Height = {}".format(lineNumber[i - 1], ifcNum, height))
       passed = False
@@ -39,19 +47,31 @@ for i, line in enumerate(lineMatch, 1):
 
 # Print Results
 if passed and warning:
-  print("\r\nRule 8 PASSED, but with the following WARNINGS \r\n{")
+  print("\r\nRule 8 PASSED, but with WARNINGS.\r\nThe following PASSED \r\n{")
+  for i, item in enumerate(passedList, 1):
+    print("     {}".format(item))
+    if i == len(passedList):
+      print("}")
+  print("\r\nThe following were WARNINGS \r\n{")
   for i, item in enumerate(warningList, 1):
     print("     {}".format(item))
     if i == len(warningList):
       print("}")
+  print("\r\nRule 8 PASSED, but with WARNINGS. Scroll up to see details.")
 elif passed:
-  print("\r\nRule 8 PASSED")
+  print("\r\nRule 8 PASSED with the following:")
+  for i, item in enumerate(passedList, 1):
+    print("     {}".format(item))
+    if i == len(passedList):
+      print("}")
+  print("\r\nRule 8 PASSED. Scroll up for details.")
 else:
   print("\r\nRule 8 FAILED for the following \r\n{")
   for i, item in enumerate(failedList, 1):
     print("     {}".format(item))
     if i == len(failedList):
       print("}")
+  print("\r\nRule 8 FAILED. Scroll up for details.")
 
 # Close the file to free up resources
 file.close()
